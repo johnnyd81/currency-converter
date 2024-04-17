79a4aa1512e41b0305ed1379b821f3f1208790ed
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-04-17 14:35:13 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 //the Header component provides information and usage instructions on how the app should be used to check currencies
+//currency data is in real time
 const Header = () => {
   return (
     <div>
